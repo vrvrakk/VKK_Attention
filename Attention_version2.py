5a8add134243bef75358b78fcb3ea82521f0b01e
vrvrakk <vrvrakk700@gmail.com>
2024-06-22 14:08:57 +0200

    german numbers now same len (all 0.5s, english and german, 24414 samplerate)

--- Attention_version2.py
+++ Attention_version2.py
@@ -62,7 +62,7 @@ def animal_sounds(noise_trials_count, idx_to_replace):
         # make sure wav_list is not empty
         if not processed_sounds:
             processed_sounds = []
-            for files in processed_files():
+            for files in processed_files:
                 processed_sound = slab.Sound.read(files)
                 processed_sounds.append(processed_sound)
         animal = random.choice(processed_sounds)
@@ -81,6 +81,7 @@ def write_buffer(chosen_voice, precomputed_animal_sounds, concatenated_animal_so
         # elements from corresponding positions are paired together
         if os.path.exists(file_path):
             s = slab.Sound(data=file_path) #.resample(samplerate=24414) # not needed for english voices
+            s.level = 77
             s.play()
             freefield.write(f'{number}', s.data, ['RX81', 'RX82'])  # loads array on buffer
             freefield.write(f'{number}_n_samples', s.n_samples, ['RX81', 'RX82'])
