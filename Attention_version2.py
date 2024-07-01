5ef8d34014c639c39deabc442d5bbc181a5fbc51
vrvrakk <vrvrakk700@gmail.com>
2024-07-01 15:06:13 +0200

    fast english and german voices added + todo test 1 animal per block from 0 azimuth

--- Attention_version2.py
+++ Attention_version2.py
@@ -150,7 +150,7 @@ def run_experiment():  # works as desired
     write_buffer(chosen_voice, selected_animal)
     run_block(trial_seq1, trial_seq2, tlo1, tlo2, s1_params, s2_params)
     return s1_delay, s2_delay, target, s1_params, s2_params, axis, block_index, chosen_voice, \
-           chosen_voice_name, tlo1, tlo2, t1_total, t2_total, streams_df, trial_seq1, trial_seq2, noise_trials_count, idx_to_replace, precomputed_animal_sounds, concatenated_animal_sounds
+           chosen_voice_name, tlo1, tlo2, t1_total, t2_total, streams_df, trial_seq1, trial_seq2, noise_trials_count, idx_to_replace, selected_animal
 
 
 if __name__ == "__main__":
@@ -159,7 +159,7 @@ if __name__ == "__main__":
     freefield.initialize('dome', device=proc_list)
     save_block_seq()  # works
     s1_delay, s2_delay, target, s1_params, s2_params, axis, block_index, chosen_voice, \
-    chosen_voice_name, tlo1, tlo2, t1_total, t2_total, streams_df, trial_seq1, trial_seq2, noise_trials_count, idx_to_replace, precomputed_animal_sounds, concatenated_animal_sounds = run_experiment()
+    chosen_voice_name, tlo1, tlo2, t1_total, t2_total, streams_df, trial_seq1, trial_seq2, noise_trials_count, idx_to_replace, selected_animal = run_experiment()
 #     # # always check speaker/processors
 #
 #
